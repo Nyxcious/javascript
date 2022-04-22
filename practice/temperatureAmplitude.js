a76cb009e44f1d070e9f0d5fd77706792a2d1828
Tayyab <mail4mrtayyab@gmail.com>
2022-04-22 20:48:15 +0500

    Update temperatureAmplitude.js

--- practice/temperatureAmplitude.js
+++ practice/temperatureAmplitude.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //!Problem
+
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate temperature amplitude
 //Keep in mind that sometimes their might be a sensor error."
 
@@ -19,23 +20,56 @@ const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 //Find the min value in temp array
 //Subtract min from max and return it = amplitude
 
+//Implementation :
+
 const calcTempAmplitude = function (temps) {
   let min = Number.MAX_SAFE_INTEGER;
   let max = Number.MIN_SAFE_INTEGER;
-  for (let i = 0; i < temperature.length; i++) {
-    if (temperature[i] === 'error') {
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] === 'error') {
       continue;
     }
-    if (temperature[i] < min) {
-      min = temperature[i];
+    if (temps[i] < min) {
+      min = temps[i];
     }
-    if (temperature[i] > max) {
-      max = temperature[i];
+    if (temps[i] > max) {
+      max = temps[i];
     }
   }
 
-  const amplitude = min - max;
+  const amplitude = max - min;
   return amplitude;
 };
 
 console.log(calcTempAmplitude(temperature));
+
+// Problem 2 : Function should now receive two arrays on temperature :
+
+//1) Understand the problem
+// With 2 arrays, should we implement same functionality twice : Answer : No, Just merge two arrays into single one
+
+//)2) Breaking into sub parts
+//How to merge two arrays into one ?
+
+//Implementation :
+
+// We can use array.concat method into , More on this on Mozilla Developer Network(MDN)
+//array3 = array1.concat(array2);
+
+// But lets try to merge on our own :
+
+const temperature2 = [-9, -4, -9, 'error', 40, 29, 15, 12, 7, 2, 'error', 3];
+
+const temperature3 = [24];
+
+for (let i = 0; i < temperature.length; i++) {
+  temperature3[i] = temperature[i];
+}
+
+let counter = 0;
+for (let i = temperature.length; i < 24; i++) {
+  temperature3[i] = temperature2[counter];
+  counter++;
+}
+
+console.log(calcTempAmplitude(temperature3));
